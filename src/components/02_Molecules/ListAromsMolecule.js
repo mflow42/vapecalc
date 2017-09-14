01a03afe9d51687f5b97960ec038c16330d27591
n.but <but.nikolay@gmail.com>
2017-09-14 17:59:06 +0300

    Next iteration of previous task

--- src/components/02_Molecules/ListAromsMolecule.js
+++ src/components/02_Molecules/ListAromsMolecule.js
@@ -8,15 +8,28 @@ class ListAromsMolecule extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: this.props.selected,
+      aroms: this.props.aromsList,
     }
   }
+  filter(e){
+    this.setState({
+      filter: e.target.value
+    })
+  }
   render() {
-    const aroms = this.props.aromsList.map((item) => <li className={'ant-select-dropdown-menu-item'} key={item} onClick={this.props.selectArom}>{item}</li>);
+    const aroms = this.props.aromsList.map((item) => <div className={'ant-select-dropdown-menu-item'} key={item} onClick={this.props.selectArom}>{item}</div>);
+    if (this.state.filter) {
+      aroms.filter(item =>
+        item.toLowerCase()
+        .includes(this.state.filter.toLowerCase())
+      )
+    }
     return (
-      <ul>{aroms}</ul>
+      <div>
+      <input type="text" onChange={this.filter.bind(this)}/>
+      {aroms}
+      </div>
     )
   }
 }
-
 export default ListAromsMolecule;
