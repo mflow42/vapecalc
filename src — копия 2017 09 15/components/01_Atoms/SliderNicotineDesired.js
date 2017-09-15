9cbc4d16bf18f1eeecd660bdba3c769f7cc99c77
n.but <but.nikolay@gmail.com>
2017-09-15 12:58:02 +0300

    Next iteration of previous task - cleaned folders accrd to atomic

--- src — копия 2017 09 15/components/01_Atoms/SliderNicotineDesired.js
+++ src — копия 2017 09 15/components/01_Atoms/SliderNicotineDesired.js
@@ -2,17 +2,20 @@ import React, {Component} from 'react';
 import {Slider} from 'antd';
 
 const marks = {
-  0: '0%',
-  10: '10%',
-  20: '20%',
-  30: '30%'
+  0: '0',
+  1: '1',
+  '1.5': '1.5',
+  3: '3',
+  6: '6',
+  9: '9',
+  12: '12 мг'
 };
 
-class SliderAroms extends Component {
-  
+class SliderNicotineDesired extends Component {
+
   state = {
-    inputValue: this.props.aromsPercent
-  };
+    inputValue: this.props.desiredNicotineStrength
+  }
 
   onChange = (value) => {
     this.setState({inputValue: value});
@@ -23,14 +26,15 @@ class SliderAroms extends Component {
       <div>
         <Slider
           marks={marks}
-          max={30}
-          defaultValue={10}
+          max={12}
+          step={0.1}
           value={this.state.inputValue}
           onChange={this.onChange}
-          onAfterChange={this.props.handleChangeAromsPercent}/>
+          onAfterChange={this.props.handleChangeNicotineDesired}
+          />
       </div>
     );
   }
 }
 
-export default SliderAroms;
+export default SliderNicotineDesired;
---
+++ src — копия 2017 09 15/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Page from '../src/components/05_Page/Page';
+import 'antd/dist/antd.css';
+import '../src/index.css';
+
+ReactDOM.render(<Page />, document.getElementById('root'));
