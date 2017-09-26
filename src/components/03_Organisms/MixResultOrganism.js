80663be1bafb5c33d08c9c5d69d8b5d7ae43e0dc
n.but <but.nikolay@gmail.com>
2017-09-26 18:10:43 +0300

    Reafactoring code done

--- src/components/03_Organisms/MixResultOrganism.js
+++ src/components/03_Organisms/MixResultOrganism.js
@@ -32,21 +32,27 @@ const COEFFICIENT_GRAMM = 1.04;
 const componentsBase = [
   {
     name: "Основы",
-    ml: (props) => (props.desiredMixVolume / (props.baseNicotineStrength / props.desiredNicotineStrength))
+    ml: (calculator) => (calculator.desiredMixVolume / (calculator.baseNicotineStrength / calculator.desiredNicotineStrength))
   }
 ];
 
-const componentAroms = [{
-  ml: (component, calculator, aroms) => ( calculator.desiredMixVolume * component.value / 100)
-}];
+// const componentAroms = [{
+//   ml: (component, calculator) => ( calculator.desiredMixVolume * component.value / 100)
+// }];
 
 const componentsPgVg = [
   {
     name: "PG",
-    ml: (calculator, aroms) => ((calculator.desiredMixVolume * calculator.desiredPgPercent / 100) - (calculator.desiredMixVolume / (calculator.baseNicotineStrength / calculator.desiredNicotineStrength) * calculator.basePgPercent / 100) - (calculator.desiredMixVolume * (calculator.aromsPercent / 100)))
+    ml: (calculator, aromTotal, componentsBaseCalculation) => (
+      (calculator.desiredMixVolume * calculator.desiredPgPercent / 100)
+      - (componentsBaseCalculation * calculator.basePgPercent / 100)
+      - aromTotal
+    )
   }, {
     name: "VG",
-    ml: (calculator, aroms) => ((calculator.desiredMixVolume * calculator.desiredVgPercent / 100) - (calculator.desiredMixVolume / (calculator.baseNicotineStrength / calculator.desiredNicotineStrength) * calculator.baseVgPercent / 100))
+    ml: (calculator) => (
+      (calculator.desiredMixVolume * calculator.desiredVgPercent / 100)
+      - (calculator.desiredMixVolume / (calculator.baseNicotineStrength / calculator.desiredNicotineStrength) * calculator.baseVgPercent / 100) )
   }
 ];
 
@@ -66,8 +72,8 @@ class MixResultOrganism extends Component {
         key: component.name,
         component: component.name,
         ml: component.ml(this.props.calculator).toFixed(2),
-        drop: (this.ml * COEFFICIENT_DROP).toFixed(0),
-        gramm: (this.ml * COEFFICIENT_GRAMM).toFixed(2)
+        drop: (component.ml(this.props.calculator) * COEFFICIENT_DROP).toFixed(0),
+        gramm: (component.ml(this.props.calculator) * COEFFICIENT_GRAMM).toFixed(2),
       }
     })
 
@@ -78,20 +84,20 @@ class MixResultOrganism extends Component {
         key: component.name,
         component: component.name,
         ml: (this.props.calculator.desiredMixVolume * this.props.aroms[index].value / 100).toFixed(2),
-        drop: (this.ml * COEFFICIENT_DROP).toFixed(0),
-        gramm: (this.ml * COEFFICIENT_GRAMM).toFixed(2)
+        drop: ((this.props.calculator.desiredMixVolume * this.props.aroms[index].value / 100) * COEFFICIENT_DROP).toFixed(0),
+        gramm: ((this.props.calculator.desiredMixVolume * this.props.aroms[index].value / 100) * COEFFICIENT_GRAMM).toFixed(2)
       }
     })
 
-    // const aromTotal = componentsAromsCalculation.reduce((arom, i) => arom += arom.ml, 0);
-    console.log(componentsAromsCalculation)
+    const aromTotal = componentsAromsCalculation.reduce((sum, current, index) => sum + +current.ml, 0);
+
     let componentsPgVgCalculation = componentsPgVg.map((component, index) => {
       return {
         key: component.name,
         component: component.name,
-        ml: component.ml(this.props.calculator, this.props.aroms).toFixed(2),
-        drop: (this.ml * COEFFICIENT_DROP).toFixed(0),
-        gramm: (this.ml * COEFFICIENT_GRAMM).toFixed(2)
+        ml: component.ml(this.props.calculator, aromTotal, componentsBaseCalculation[0].ml).toFixed(2),
+        drop: (component.ml(this.props.calculator, aromTotal, componentsBaseCalculation[0].ml) * COEFFICIENT_DROP).toFixed(0),
+        gramm: (component.ml(this.props.calculator, aromTotal, componentsBaseCalculation[0].ml) * COEFFICIENT_GRAMM).toFixed(2)
       }
     })
 
@@ -99,9 +105,9 @@ class MixResultOrganism extends Component {
       return {
         key: component.name,
         component: component.name,
-        ml: component.ml(this.props.calculator, this.props.aroms).toFixed(2),
-        drop: (this.ml * COEFFICIENT_DROP).toFixed(0),
-        gramm: (this.ml * COEFFICIENT_GRAMM).toFixed(2)
+        ml: (+componentsBaseCalculation[0].ml + aromTotal + +componentsPgVgCalculation[0].ml + +componentsPgVgCalculation[1].ml).toFixed(2),
+        drop: ((+componentsBaseCalculation[0].ml + aromTotal + +componentsPgVgCalculation[0].ml + +componentsPgVgCalculation[1].ml) * COEFFICIENT_DROP).toFixed(0),
+        gramm: ((+componentsBaseCalculation[0].ml + aromTotal + +componentsPgVgCalculation[0].ml + +componentsPgVgCalculation[1].ml) * COEFFICIENT_GRAMM).toFixed(2)
       }
     })
 
